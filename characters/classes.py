3799e503d9bf20f29f2bc7e03fc40daf65eaafa9
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-01-25 20:17:53 +0530

    Update classes.py

--- characters/classes.py
+++ characters/classes.py
@@ -40,7 +40,8 @@ class person:
             return self.hp
     
     def reduce_xp(self,change):
-        self.xp-=change
+        self.xp=int(self.xp)-int(change)
+        return self.xp
 
     def get_id(self):
         return self.id
@@ -72,5 +73,42 @@ class _member:
     def get_member_hp_level(self):
         return self.hp_level
     
+
+class team:
+
+    def __init__(self,name,leader_id):
+        self.team_members=[]
+        self.team_name=name
+        self.team_leader=leader_id
+
+    def add_member(self,member):
+        self.team_members.append(member)
+    
+    def remove_member(self,member):
+        self.team_members.remove(member)
+
+    def get_team_name(self):
+        return self.team_name
+        
+    def get_team_members(self):
+        return self.team_members
+
+    def get_leader_id(self):
+        return self.team_leader
+
+    def change_leader_id(self,id):
+        self.team_leader=id
+
+class teamplayer(person):
+
+    def __init__(self,hp,xp,name,damage,id,status):
+        person.__init__(self,hp,xp,name,damage,id,status)
+        self.team="none"
+
+    def get_team(self):
+        return self.team
+    
+    def take_team(self,team_name):
+        self.team=team_name
 
 
